f66a345e8dd219eb74f3bdebb1ac40b135c4bae3
mahesh-143 <maheshodedara13@yahoo.com>
2022-05-28 16:53:59 +0530

    added cors

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express()
+const cors = require("cors")
 
 require("dotenv/config")
 require("./config/db.js")
@@ -7,7 +8,7 @@ require("./config/db.js")
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// app.use(require('cors')())
+app.use(cors())
 app.use('/api', require('./routes'))
 app.use((error, req, res, next) => {    
     console.log(error);
