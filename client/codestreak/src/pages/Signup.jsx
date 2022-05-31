55f27b61bcb1d7048b4e277b2f86890aa417dd02
mahesh-143 <maheshodedara13@yahoo.com>
2022-05-31 17:48:54 +0530

    added user registration

--- client/codestreak/src/pages/Signup.jsx
+++ client/codestreak/src/pages/Signup.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react'
 import { Button } from "../components/styles/Button.styled"
 import  { Link } from "react-router-dom"
+import axios from 'axios'
 
 export const Signup = () => {
 
     const [user, setUser] = useState("")
+
+    const url = `http://127.0.0.1:5000/api/auth/signup`;
 
     const handleChange= (e) => {
         const name = e.target.name
@@ -14,18 +17,30 @@ export const Signup = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault()
-       
+        try { 
+            console.log(user)
+            const response = await axios.post(url, 
+                JSON.stringify({...user}),
+                {
+                    headers : { 'Content-Type' : 'application/json'}
+                })
+                console.log(response.data)
+            
+        }
+        catch(error){
+            console.log(`Error : ${error}`)
+        }
     }
   return (
     <>
     <h1>Sign Up</h1>
     <form  onSubmit={handleSubmit}>
-        <label htmlFor="userName">username</label>
+        <label htmlFor="name">username</label>
         <input
             type="text"
-            name="username"
-            id="userName"
-            value={user.username || ""}
+            name="name"
+            id="name"
+            value={user.name || ""}
             onChange={handleChange}
         />
         <label htmlFor="email">email</label>
